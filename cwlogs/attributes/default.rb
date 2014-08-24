e28c016ae961c008d9711cbd50d2f58fca56eeb7
Stephanie King <stepking@amazon.com>
2014-08-24 13:04:10 -0700

    changed logfiles structure

--- cwlogs/attributes/default.rb
+++ cwlogs/attributes/default.rb
@@ -2,15 +2,15 @@
 
 default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log'
 
-default[:cwlogs][:logfiles] = ['/var/log/aws/opsworks/opsworks-agent.log']
+default[:cwlogs][:logfiles] = {}
 default[:cwlogs][:installer] = "awslogs-agent-setup.py"
 default[:cwlogs][:installer_url] = "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/#{node[:cwlogs][:installer]}"
 default[:cwlogs][:directory] = "/opt/aws/cloudwatch"
 default[:cwlogs][:cw_region] = "us-east-1"
 
 
-default[:cwlogs][:logfiles][:opsworks_agent] = "/var/log/aws/opsworks/opsworks-agent.log"
-default[:cwlogs][:logfiles][:messages] = "/var/log/messages"
-default[:cwlogs][:logfiles][:error_log] = "/var/log/httpd/error_log"
-default[:cwlogs][:logfiles][:access_log] = "/var/log/httpd/access_log"
+default[:cwlogs][:logfiles]["opsworks_agent"] = "/var/log/aws/opsworks/opsworks-agent.log"
+default[:cwlogs][:logfiles]["messages"] = "/var/log/messages"
+default[:cwlogs][:logfiles]["error_log"] = "/var/log/httpd/error_log"
+default[:cwlogs][:logfiles]["access_log"] = "/var/log/httpd/access_log"
 
