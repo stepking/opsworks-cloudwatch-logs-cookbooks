c3e0ffcf37348711b5b5359a92d39f31fad82983
Stephanie King <stepking@amazon.com>
2014-08-24 13:14:02 -0700

    fixed template format and default logs

--- cwlogs/attributes/default.rb
+++ cwlogs/attributes/default.rb
@@ -2,15 +2,19 @@
 
 default[:cwlogs][:logfile] = '/var/log/aws/opsworks/opsworks-agent.log'
 
-default[:cwlogs][:logfiles] = {}
 default[:cwlogs][:installer] = "awslogs-agent-setup.py"
 default[:cwlogs][:installer_url] = "https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/#{node[:cwlogs][:installer]}"
 default[:cwlogs][:directory] = "/opt/aws/cloudwatch"
+
+#currently only cwlogs has an endpoint in us-east-1
 default[:cwlogs][:cw_region] = "us-east-1"
 
+#used to add more logs to monitor
+default[:cwlogs][:logfiles] = {}
 
+#amazon linux
 default[:cwlogs][:logfiles]["opsworks_agent"] = "/var/log/aws/opsworks/opsworks-agent.log"
 default[:cwlogs][:logfiles]["messages"] = "/var/log/messages"
-default[:cwlogs][:logfiles]["error_log"] = "/var/log/httpd/error_log"
-default[:cwlogs][:logfiles]["access_log"] = "/var/log/httpd/access_log"
+default[:cwlogs][:logfiles]["error_log"] = "/var/log/httpd/error.log"
+default[:cwlogs][:logfiles]["access_log"] = "/var/log/httpd/access.log"
 
