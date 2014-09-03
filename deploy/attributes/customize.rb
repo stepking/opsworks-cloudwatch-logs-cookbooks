0c6242a4413e34b646afe4b4fbf4859ad083e035
Stephanie King <stepking@amazon.com>
2014-09-03 12:06:05 -0700

    fixed syntax error

--- deploy/attributes/customize.rb
+++ deploy/attributes/customize.rb
@@ -1,3 +1,3 @@
 node[:opsworks][:applications].each do |application|
-  normal[:deploy][application[:name]][:deploy_to] = "/var/log/#{application[:deploy_to]"
+  normal[:deploy][application[:name]][:deploy_to] = "/var/log/#{application[:deploy_to]}"
 end
